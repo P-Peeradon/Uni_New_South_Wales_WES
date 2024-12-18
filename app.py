8e47a71a1f4952787353cdc821c5ec250b0a6025
P-Peeradon <p.sootsukon61@gmail.com>
2024-12-18 18:05:02 +1100

    Finishing login template

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "<h1>Hello Flask Framework</h1>"
+
+@app.route('/<name>')
+def greet(name):
+    return f"Hola, {name}"
+    
 if __name__ == "__main__":
     app.run()
